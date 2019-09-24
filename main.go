40556a167a37a479fc2bbfd3b11ca335d9aa2b1c
Dries De Peuter <dries@nousefreak.be>
2019-09-24 21:25:36 +0200

    Add documentation

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package vembed
 
 var gitCommit, gitBranch, gitState, gitSummary, buildDate, version string
 
+// Version acts as a way to access the injected version information.
 var Version Vembed
 
+// Vembed represents the version of the package.
 type Vembed struct {
 	gitCommit  string
 	gitBranch  string
@@ -13,30 +15,42 @@ type Vembed struct {
 	version    string
 }
 
+// GetGitCommit return the git commit.
 func (v *Vembed) GetGitCommit() string {
 	return v.gitCommit
 }
+
+// GetGitBranch return the git branch
 func (v *Vembed) GetGitBranch() string {
 	return v.gitBranch
 }
+
+// GetGitState returns the git state.
 func (v *Vembed) GetGitState() string {
 	return v.gitState
 }
+
+// GetGitSummary returns the git summary.
 func (v *Vembed) GetGitSummary() string {
 	return v.gitSummary
 }
+
+// GetBuildDate returns the build date.
 func (v *Vembed) GetBuildDate() string {
 	return v.buildDate
 }
+
+// GetVersion returns the version.
 func (v *Vembed) GetVersion() string {
 	return v.version
 }
 
+// String returns the version.
 func (v *Vembed) String() string {
 	if v.version == "" {
 		return "unknown"
 	}
-	return v.version
+	return v.GetVersion()
 }
 
 func init() {
